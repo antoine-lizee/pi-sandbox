80a23cd58697d08106e5cc86828695fd255d3236
antoine-lizee-pi <antoine.lizee@gmail.com>
2017-07-12 08:25:12 +0000

    enh: setup jupyter to run in /notebooks

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,3 +2,4 @@ c.NotebookApp.open_browser = False
 c.NotebookApp.port = 8910
 c.NotebookApp.ip = '*'
 c.NotebookApp.password = 'sha1:8b04d81e7ba2:5e886e4e567139692de43776476a033b2b1d2d22'
+c.NotebookApp.notebook_dir = 'notebooks'
